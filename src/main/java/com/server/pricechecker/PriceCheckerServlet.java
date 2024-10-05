ad65a5a045e6a3ff46574af102a881b77af90428
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-10-05 07:21:07 +0530

    made changes on other branches with maven

---
+++ src/main/java/com/server/pricechecker/PriceCheckerServlet.java
@@ -0,0 +1,21 @@
+package com.server.pricechecker;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+//@WebServlet(name= "Initial Servlet", urlPatterns = "/price-checker")
+public class PriceCheckerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().write("<h1>Price Checker is working!</h1>");
+    }
+}
